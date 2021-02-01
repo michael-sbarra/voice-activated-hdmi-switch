c501e8a4bdfa9b69a68b7611724f428668c0ac63
Michael Sbarra <michael.p.sbarra.software@gmail.com>
2021-01-31 23:52:55 -0800

    syntax changes

--- pysrc/hdmi_switcher_server.py
+++ pysrc/hdmi_switcher_server.py
@@ -7,17 +7,21 @@ from flask import Flask, request
 import logging
 from logging.config import dictConfig
 
-config_file = os.path.join(os.path.dirname(__file__),
-                '../config/serial-settings.json')
+config_file = os.path.join \
+    (
+        os.path.dirname(__file__),
+        '../config/serial-settings.json'
+    )
 
 if not os.path.isfile(config_file):
-    raise RuntimeError("Config file {0} not found".format(config_file))
+    raise RuntimeError(f"Config file {config_file} not found")
 
 with open(config_file) as f:
     config = json.load(f)
-    print('Loaded config file {0}'.format(config_file))
+    print(f"Loaded config file {config_file}")
 
-dictConfig({
+dictConfig \
+({
     'version': 1,
     'formatters': {'default': {
         'format': '[%(asctime)s] %(levelname)-7s %(module)-22s: %(message)s',
@@ -35,49 +39,53 @@ dictConfig({
 
 app = Flask(__name__)
 
+def format_keys(keys):
+    return "'{0}'".format("', '".join(map(str, keys)))
 
 @app.route('/', methods=['POST'])
 def handle_serial():
     request_json = request.get_json()
-    app.logger.debug('Request JSON {0}'.format(request_json))
+    app.logger.debug(f"Request JSON {request_json}")
 
     with open(config_file) as f:
         config = json.load(f)
-    
+
     log_level = 'ERROR'
     if u'device' not in request_json.keys():
-        response = "FAILURE. Key 'device' not in JSON payload {0}".format(
-            request_json)
+        response = f"FAILURE. Key 'device' not in JSON payload {request_json}"
     else:
         selected_device_name = request_json[u'device'].lower()
 
         if selected_device_name not in config['hdmi_devices_map']:
-            response = "FAILURE. Requested device '{0}' not defined. Please " \
-                "select from '{1}'".format(selected_device_name, 
-                "', '".join(list(config['hdmi_devices_map'].keys())))
+            response = f"FAILURE. Requested device '{selected_device_name}' not defined. " \
+                f"Please select from {format_keys(config['hdmi_devices_map'].keys())}"
         else:
+
             selected_device_idx = config['hdmi_devices_map'][selected_device_name]
-            serial_str_out = 'sw i0{0}\r\n'.format(selected_device_idx)
+            serial_str_out = f"sw i0{selected_device_idx}\r\n"
+
+            device  = config['serial_device']['port']
+            baud    = config['serial_device']['baud']
+            timeout = config['serial_device']['timeout']
 
             try:
-                with serial.Serial(
-                        config['serial_device']['port'], 
-                        config['serial_device']['baud'],
-                        timeout=config['serial_device']['timeout']) as ser:
-                    app.logger.debug('{0} Opened'.format(ser.portstr))
+                with serial.Serial(device, baud, timeout=timeout) as ser:
+                    app.logger.debug(f"{ser.portstr} Opened")
                     ser.write(str.encode(serial_str_out))
-                    app.logger.info("SUCCESS - Wrote {0}".format(
-                        repr(serial_str_out)))
-                    response = "SUCCESS. Changed HDMI to device '{0}'".format(
-                        selected_device_name)
+                    app.logger.info(f"SUCCESS - Wrote {serial_str_out!r}")
+                    response = f"SUCCESS. Changed HDMI to device '{selected_device_name}'"
                     log_level = 'INFO'
-                app.logger.debug('{0} Closed'.format(ser.portstr))
+                app.logger.debug(f"{ser.portstr} Closed")
             except serial.serialutil.SerialException as err:
                 app.logger.error(err)
-                response = 'FAILURE. Serial device not available'
+                response = "FAILURE. Serial device not available"
 
     app.logger.log(getattr(logging, log_level), response)
     return response
 
 if __name__ == '__main__':
-    app.run(config['server']['IP'], config['server']['port'])
+    app.run \
+    (
+        config['server']['IP'],
+        config['server']['port']
+    )
